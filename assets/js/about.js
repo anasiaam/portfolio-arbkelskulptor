ac5e6d74b8a6972b33a09133ffcdffc7baa212b3
anasiaam <anasiameshaj13@gmail.com>
2026-02-22 23:39:50 +0100

    Clean reconnect

---
+++ assets/js/about.js
@@ -0,0 +1,9 @@
+new Swiper(".slider", {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    effect: "slide", // or "fade" (see below)
+    speed: 800,
+});
